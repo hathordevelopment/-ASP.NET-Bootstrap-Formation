66b6554b33f56537a26180001a1465fdcc2ff640
Alexis Goupillot <hathor.development@gmail.com>
2018-06-27 14:38:19 +0200

    /!\ Point de départ de la formation

---
+++ AvisFormation.WebUi/Controllers/FormationController.cs
@@ -0,0 +1,23 @@
+﻿using AvisFormation.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AvisFormation.WebUi.Controllers
+{
+    public class FormationController : Controller
+    {
+        // GET: Formation
+        public ActionResult ToutesLesFormations()
+        {
+            using (var context = new AvisEntities())
+            {
+                var listeFormations = context.Formations.ToList();
+            }
+
+            return View();
+        }
+    }
+}
